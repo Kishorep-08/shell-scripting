ae2cc0758314905b00c435554a0b0d8f461853b8
kishorep-08 <kishore.pkdp@gmail.com>
2025-09-18 21:55:54 +0530

    Shell-script

---
+++ 01-shell-sample.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+##################################
+Author: Kishore P
+Date: 18-09-2025
+Desc: This is sample shell script
+##################################
+
+echo "I am Learning DevOps"
