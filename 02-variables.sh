739e271d596fc4a704cd9f24d5b579aa4a3d6162
kishorep-08 <kishore.pkdp@gmail.com>
2025-09-20 21:00:59 +0530

    updated

---
+++ 02-variables.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+######################################
+#Author: Kishore
+#Date: 20-09-2025
+#Version: 1.0
+#Description: Practicing variables
+######################################
+
+echo "What are you learning?"
+
+read COURSE
+
+echo "The course is $COURSE"
+
+
+
