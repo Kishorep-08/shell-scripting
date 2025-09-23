df154f6cddfae4eac360d8fb62afced209d574f4
kishorep-08 <kishore.pkdp@gmail.com>
2025-09-23 20:00:53 +0530

    datatypes

---
+++ data-types.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+a=36
+b=39
+
+sum=$(($a+$b))
+echo Sum is $sum
+echo
+
+#Array
+
+Preferred_locations=("Hyderabad" "Chennai" "Bangalore")
+
+echo "My preferred locations are ${Preferred_location[@]}"
