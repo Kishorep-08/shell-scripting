2e869878c6dd4a56b0a75e3452dc53d7c2a27b14
kishorep-08 <kishore.pkdp@gmail.com>
2025-09-24 22:27:36 +0530

    updated

---
+++ 12-installations.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "Error: Install with root privileges"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "Error: Installing $2 got failed"
+        exit 1
+    else
+        echo "Installation of $2 is Success"
+    fi
+}
+
+dnf install mysql -y
+
+VALIDATE $? "Mysql"
+
+dnf install nginx -y
+
+VALIDATE $? "Nginx"
+
+dnf install mongodb-mongosh -y
+
+VALIDATE $? "mongosh"
