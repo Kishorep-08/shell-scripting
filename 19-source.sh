192721346eae8939b4c8b97e1d5106ade14cfade
kishorep-08 <kishore.pkdp@gmail.com>
2025-09-28 08:18:42 +0530

    source

---
+++ 19-source.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+NAME=INDIA
+
+echo "Welcome to $NAME"
+
+echo "PID of $0 is $$"
+
+sh 20-source.sh
---
+++ 20-source.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Welcome to $NAME"
+
+echo "PID of $0 script is $$"
