137f9b6e95691b521bb567cdcb8e58ca353bd320
kishorep-08 <kishore.pkdp@gmail.com>
2025-09-28 09:29:26 +0530

    source

--- 19-source.sh
+++ 19-source.sh
@@ -4,6 +4,6 @@ NAME=INDIA
 
 echo "Welcome to $NAME"
 
-echo "PID of 19-script.sh is $$"
+echo "PID of 19-source.sh is $$"
 
 source ./20-source.sh
--- 20-source.sh
+++ 20-source.sh
@@ -2,4 +2,4 @@
 
 echo "Welcome to $NAME"
 
-echo "PID of 20-script.sh script is $$"
+echo "PID of 20-source.sh script is $$"
