99386f2313a968d71f43159f73249314e80c94c3
kishorep-08 <kishore.pkdp@gmail.com>
2025-09-29 12:05:55 +0530

    loops

--- 21-while.sh
+++ 21-while.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-num=10
+#################################
 
-while [ $num -ge 5 ]
-do
-    echo "Number is: $num"
-    num=$(($num-1))
+# num=10
+# while [ $num -ge 5 ]
+# do
+#     echo "Number is: $num"
+#     num=$(($num-1))
+# done
+
+#################################
+
+number=9
+while [ $number -le 10 ]
+do 
+    echo "Number is : $number"
+    number=$(($number+1))
 done
