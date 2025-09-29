fb5a4e3fe98c425b247f7d86b5eec44d59365cac
kishorep-08 <kishore.pkdp@gmail.com>
2025-09-29 12:41:53 +0530

    loops

--- 22-while.sh
+++ 22-while.sh
@@ -1,8 +1,15 @@
-#!/bin/bash
+# #!/bin/bash
 
-##Script to print each line one by one in a file
+# ##Script to print each line one by one in a file
 
-while read content      #each line is stored in content and printed
+# while read content      #each line is stored in content and printed
+# do
+#     echo "Line is: $content"
+# done < file-1.txt       #this is the file. It will be considered input
+
+
+
+while IFS= read -r line
 do
-    echo "Line is: $content"
-done < file-1.txt       #this is the file. It will be considered input
+    echo "Line is: $line"
+done < file-2.txt
