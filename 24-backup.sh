498242b9976c08e29f5c481412eceff17fbd6704
kishorep-08 <kishore.pkdp@gmail.com>
2025-09-29 15:11:54 +0530

    logs

--- 24-backup.sh
+++ 24-backup.sh
@@ -5,6 +5,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-script-logs"
 SCRIPT_NAME=$(echo "$0" | awk -F. '{print$1}')
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
