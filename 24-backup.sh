3d3e9973022ef2946c58e9818e2d63def12d471a
kishorep-08 <kishore.pkdp@gmail.com>
2025-09-29 15:10:22 +0530

    logs

---
+++ 24-backup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-script-logs"
+SCRIPT_NAME=$(echo "$0" | awk -F. '{print$1}')
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SOURCE_DIR=$1
+DEST_DIR=$2
+
+mkdir -p $LOGS_FOLDER
+
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R Error $N: Install with root privileges" | tee -a $LOG_FILE
+    exit 1
+fi
+
+if [ $# -lt 2 ]
+then
+    echo -e "$R USAGE: sudo sh 24-backup.sh <source_dir> <destination_dir> <days>(optional) $N" | tee -a $LOG_FILE
+    exit 1
+fi
+
+if [ ! -d $SOURCE_DIR ]
+then
+    echo -e "$R $SOURCE_DIR doesn't exist $N" | tee -a $LOG_FILE
+fi
